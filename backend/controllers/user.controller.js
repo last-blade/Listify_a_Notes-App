dd6ebe3b93f0cd20c6fffe26ff7720b9e5564b87
Prashant <meprashanttyagi2000@gmail.com>
2024-09-22 12:20:37 +0530

    some bugs fixing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,41 +1,55 @@
-import { apiError } from "../utils/apiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import User from "../models/user.model.js"
-import {apiResponse} from "../utils/apiResponse.js"
+import { apiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js"
+import { apiResponse } from "../utils/apiResponse.js"
 
-const registerUser = asyncHandler(async (request, response)=>{
-    const {fullname, email, password} = request.body;
+const registerUser = asyncHandler( async (request, response) => {
+    const {email, fullname, password} = request.body;
+    console.log("email:- ", email);
+    // check kar rahe hain ki saare fields bhare huye ho user dwara like email, password, etc. yeh sab bahara hona chahiye
+    /*if(fullname === ""){
+        throw new apiError(400, "Fullname is required.");        // 'apiError' naam se file banayi hai utils ke folder mein 
+                                                                or usmein jaake dekhoge toh humne constructor banaya hai 
+                                                                or constructor mein statuscode and message naam kaa field 
+                                                                hai, isliye maine status code(i.e. 400) and message(i.e. Fullname is required) 
+                                                                paas kiya hai ismein.
+                                                            
+    lekin aise toh hum ko har filed ke liye if-condition lagani paegi i.e. password, fullname, 
+    email, username, etc. toh ise code lamba ho jaayega, toh iski jagah par hum if condition mein 'some' naam kaa method use karenge
+    see below:-                                                        
+    }*/ 
 
-    console.log("Fullname:- ", fullname);
-
-    if([fullname, email, password].some((anyField)=>anyField?.trim() === "")){
+    if([fullname, password, email].some((anyfield)=> anyfield?.trim() === "")){
         throw new apiError(400, "All fields are required.")
-    }
+    };
 
-    const existedUser = User.findOne({
-        $or: [{email}]
-    })
-
+    /*const existedUser = User.findOne({email});    database mein user ko find kar rahe hain uski email id ke through, lekin hum doosre
+                                                    tareeke se karenge check*/
+                                                    
+    const existedUser = await User.findOne({
+        $or: [{email}], // '$or'-> The $or operator is used to specify that either one of the conditions should match.
+    });
+    
     if(existedUser){
-        throw new apiError(409, `User with this email ${email} already exist.`)
+        throw new apiError(409, "User with email or username already exist.")
     }
 
     const user = await User.create({
         fullname,
         email,
-        password
+        password,
     });
 
-    const createdUser = await User.findOne(user._id).select("-password -refreshToken");
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
 
     if(!createdUser){
-        throw new apiError(500, "Server error whle registering the user.")
+        throw new apiError(500, "Server error while registering the user.");
     }
 
     return response.status(201).json(
         new apiResponse(201, createdUser, "User registered successfully.")
     )
 
-})
+});
 
-export default registerUser;
+export {registerUser};
