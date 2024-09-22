1e729a8c266101297d14a46cda2ee29f8dd7aac7
Prashant <meprashanttyagi2000@gmail.com>
2024-09-22 12:21:05 +0530

    fixed errors

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -1,14 +1,27 @@
+// 1. promises wala tareeka
+
+// const asyncHandler = (requestHandler) => {
+//     return (request, response, next) => {
+//         Promise.resolve(requestHandler(request, response, next)).catch((error) => next(error))
+//     }
+// }
+
+
+
+
+//2. try catch wala tareeka
 const asyncHandler = (func) => async (request, response, next) => {
     try {
-        await func(request, response, next)    
+        await func(request, response, next)
     } 
     
     catch (error) {
         response.status(error.code || 500).json({
             message: error.message,
             success: false,
-        })
+        })    
     }
 }
 
-export {asyncHandler};
+
+export { asyncHandler }
